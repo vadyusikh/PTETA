09767f0a7662a82a0fdfaf620dda0bfd7d54486a
vadimh <vadim.honcharenko@sytoss.com>
2022-11-13 11:33:08 +0200

    fix some issues in RouteENU

--- PTETA/utils/RouteENU.py
+++ PTETA/utils/RouteENU.py
@@ -13,7 +13,7 @@ class RouteENU:
         self.stops_forward = [BusStop(stp) for stp in route_info['stops']['forward']]
         self.stops_backward = [BusStop(stp) for stp in route_info['stops']['backward']]
 
-        self.track_forward = get_track_enu(route_info['scheme']['backward'], center)
+        self.track_forward = get_track_enu(route_info['scheme']['forward'], center)
         self.track_backward = get_track_enu(route_info['scheme']['backward'], center)
 
     def get_projection_on_track(self, points: np.array, is_geod:bool = True) -> RouteProjection:
