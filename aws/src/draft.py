56d15243feef1472e9248b5e89534ae8fa2567e1
vadimh <vadim.honcharenko@sytoss.com>
2022-12-02 02:45:16 +0200

    init AWS RDS PotsgresSQL

---
+++ aws/src/draft.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+
+import os
+
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
